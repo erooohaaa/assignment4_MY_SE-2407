01c8e5acdda8406c81151f0a96a7c5d0ea54b924
Yerkanat <erkanatm249@gmail.com>
2025-04-26 13:14:36 +0500

    the project's structure

--- src/BST.java
+++ src/BST.java
@@ -1,2 +1,32 @@
-public class BST {
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+
+    private class Node {
+        private K key;
+        private V val;
+        private Node left;
+        private Node right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    public void put(K key, V val) {
+        // Тупая заглушка
+        throw new UnsupportedOperationException("Метод не реализован");
+    }
+
+    public V get(K key) {
+
+    }
+
+    public void delete(K key) {
+
+    }
+
+    public Iterable<K> iterator() {
+
+    }
 }

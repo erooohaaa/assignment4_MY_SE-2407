9c848c86fa88e4ec1463b7bf42fa2fe72951bae4
Yerkanat <erkanatm249@gmail.com>
2025-04-28 22:02:44 +0500

    annotations and comments (Visual)

--- src/BST.java
+++ src/BST.java
@@ -1,146 +1,234 @@
-    import java.util.Iterator;
-    import java.util.NoSuchElementException;
-    import java.util.Stack;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
 
-    public class BST<K extends Comparable<K>, V> implements Iterable<BST<K, V>.NodeEntry> {
-        private Node root;
-        private int size;
+/**
+ * A generic Binary Search Tree (BST) implementation that supports operations
+ * like insertion, deletion, and searching for key-value pairs.
+ *
+ * @param <K> the type of keys (must be Comparable)
+ * @param <V> the type of values
+ */
+public class BST<K extends Comparable<K>, V> implements Iterable<BST<K, V>.NodeEntry> {
 
-        public class Node {
-            private K key;
-            private V val;
-            private Node left, right;
+    private Node root;  // Root of the binary search tree
+    private int size;   // Size of the tree (number of nodes)
 
-            public Node(K key, V val) {
-                this.key = key;
-                this.val = val;
-            }
-        }
+    /**
+     * Node class that represents each element in the BST.
+     */
+    public class Node {
+        private K key;    // Key of the node
+        private V val;    // Value associated with the key
+        private Node left, right;  // Left and right child nodes
 
-        public class NodeEntry {
-            private final K key;
-            private final V value;
-
-            public NodeEntry(K key, V value) {
-                this.key = key;
-                this.value = value;
-            }
-
-            public K getKey() {
-                return key;
-            }
-
-            public V getValue() {
-                return value;
-            }
-        }
-
-        public void put(K key, V val) {
-            root = put(root, key, val);
-        }
-
-        private Node put(Node node, K key, V val) {
-            if (node == null) {
-                size++;
-                return new Node(key, val);
-            }
-            int cmp = key.compareTo(node.key);
-            if (cmp < 0) {
-                node.left = put(node.left, key, val);
-            } else if (cmp > 0) {
-                node.right = put(node.right, key, val);
-            } else {
-                node.val = val;
-            }
-            return node;
-        }
-
-        public V get(K key) {
-            Node node = root;
-            while (node != null) {
-                int cmp = key.compareTo(node.key);
-                if (cmp < 0) {
-                    node = node.left;
-                } else if (cmp > 0) {
-                    node = node.right;
-                } else {
-                    return node.val;
-                }
-            }
-            return null;
-        }
-
-        public void delete(K key) {
-            root = delete(root, key);
-        }
-
-        private Node delete(Node node, K key) {
-            if (node == null) return null;
-            int cmp = key.compareTo(node.key);
-            if (cmp < 0) {
-                node.left = delete(node.left, key);
-            } else if (cmp > 0) {
-                node.right = delete(node.right, key);
-            } else {
-                size--;
-                if (node.right == null) return node.left;
-                if (node.left == null) return node.right;
-                Node t = node;
-                node = min(t.right);
-                node.right = deleteMin(t.right);
-                node.left = t.left;
-            }
-            return node;
-        }
-
-        private Node min(Node node) {
-            if (node.left == null) return node;
-            else return min(node.left);
-        }
-
-        private Node deleteMin(Node node) {
-            if (node.left == null) return node.right;
-            node.left = deleteMin(node.left);
-            return node;
-        }
-
-        public int size() {
-            return size;
-        }
-
-        @Override
-        public Iterator<NodeEntry> iterator() {
-            return new BSTIterator();
-        }
-
-        private class BSTIterator implements Iterator<NodeEntry> {
-            private Stack<Node> stack = new Stack<>();
-
-            public BSTIterator() {
-                pushLeft(root);
-            }
-
-            private void pushLeft(Node node) {
-                while (node != null) {
-                    stack.push(node);
-                    node = node.left;
-                }
-            }
-
-            @Override
-            public boolean hasNext() {
-                return !stack.isEmpty();
-            }
-
-            @Override
-            public NodeEntry next() {
-                if (!hasNext()) throw new NoSuchElementException();
-                Node node = stack.pop();
-                NodeEntry entry = new NodeEntry(node.key, node.val);
-                if (node.right != null) {
-                    pushLeft(node.right);
-                }
-                return entry;
-            }
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
         }
     }
+
+    /**
+     * NodeEntry class used to return key-value pairs while iterating over the tree.
+     */
+    public class NodeEntry {
+        private final K key;  // Key of the entry
+        private final V value; // Value of the entry
+
+        public NodeEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+    /**
+     * Inserts a key-value pair into the BST.
+     * If the key already exists, updates the associated value.
+     *
+     * @param key the key to insert
+     * @param val the value associated with the key
+     */
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    /**
+     * Helper method for inserting a key-value pair recursively.
+     *
+     * @param node the current node
+     * @param key the key to insert
+     * @param val the value to associate with the key
+     * @return the updated node
+     */
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            size++;
+            return new Node(key, val);  // Insert a new node if the tree is empty at this position
+        }
+        int cmp = key.compareTo(node.key);  // Compare keys to navigate the tree
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);  // Insert in the left subtree
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);  // Insert in the right subtree
+        } else {
+            node.val = val;  // Update the value if the key already exists
+        }
+        return node;
+    }
+
+    /**
+     * Retrieves the value associated with the given key.
+     *
+     * @param key the key to search for
+     * @return the value associated with the key, or null if not found
+     */
+    public V get(K key) {
+        Node node = root;
+        while (node != null) {
+            int cmp = key.compareTo(node.key);
+            if (cmp < 0) {
+                node = node.left;  // Move to the left subtree
+            } else if (cmp > 0) {
+                node = node.right;  // Move to the right subtree
+            } else {
+                return node.val;  // Return the value if the key is found
+            }
+        }
+        return null;  // Return null if the key is not found
+    }
+
+    /**
+     * Deletes the key-value pair associated with the given key.
+     *
+     * @param key the key to delete
+     */
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    /**
+     * Helper method for deleting a key-value pair recursively.
+     *
+     * @param node the current node
+     * @param key the key to delete
+     * @return the updated node after deletion
+     */
+    private Node delete(Node node, K key) {
+        if (node == null) return null;  // Key not found, return null
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = delete(node.left, key);  // Search in the left subtree
+        } else if (cmp > 0) {
+            node.right = delete(node.right, key);  // Search in the right subtree
+        } else {
+            size--;  // Decrease the size of the tree as we are deleting a node
+            if (node.right == null) return node.left;  // No right child, return the left child
+            if (node.left == null) return node.right;  // No left child, return the right child
+            Node t = node;
+            node = min(t.right);  // Find the minimum node in the right subtree
+            node.right = deleteMin(t.right);  // Delete the minimum node from the right subtree
+            node.left = t.left;  // Link the left child of the deleted node to the new node
+        }
+        return node;
+    }
+
+    /**
+     * Finds the minimum node in the given subtree.
+     *
+     * @param node the root node of the subtree
+     * @return the minimum node
+     */
+    private Node min(Node node) {
+        if (node.left == null) return node;  // Minimum node is the leftmost node
+        else return min(node.left);
+    }
+
+    /**
+     * Deletes the minimum node in the given subtree.
+     *
+     * @param node the root node of the subtree
+     * @return the updated subtree with the minimum node removed
+     */
+    private Node deleteMin(Node node) {
+        if (node.left == null) return node.right;  // If left child is null, return the right child
+        node.left = deleteMin(node.left);  // Recursively delete the minimum node from the left subtree
+        return node;
+    }
+
+    /**
+     * Returns the size of the tree (number of nodes).
+     *
+     * @return the size of the tree
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Returns an iterator to iterate through the BST in order.
+     *
+     * @return an iterator
+     */
+    @Override
+    public Iterator<NodeEntry> iterator() {
+        return new BSTIterator();
+    }
+
+    /**
+     * BSTIterator class to traverse the tree in order (in-order traversal).
+     */
+    private class BSTIterator implements Iterator<NodeEntry> {
+        private Stack<Node> stack = new Stack<>();  // Stack to hold the nodes for in-order traversal
+
+        public BSTIterator() {
+            pushLeft(root);  // Push all the left nodes from the root
+        }
+
+        /**
+         * Helper method to push all left nodes onto the stack.
+         *
+         * @param node the node to start pushing from
+         */
+        private void pushLeft(Node node) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;  // Move to the left child
+            }
+        }
+
+        /**
+         * Checks if there are more elements to iterate over.
+         *
+         * @return true if there are more nodes to visit, false otherwise
+         */
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        /**
+         * Returns the next node in the in-order traversal.
+         *
+         * @return the next NodeEntry in the traversal
+         * @throws NoSuchElementException if there are no more elements
+         */
+        @Override
+        public NodeEntry next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Node node = stack.pop();  // Pop the top node from the stack
+            NodeEntry entry = new NodeEntry(node.key, node.val);  // Create a NodeEntry for the current node
+            if (node.right != null) {
+                pushLeft(node.right);  // Push all left nodes of the right child onto the stack
+            }
+            return entry;
+        }
+    }
+}
--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,14 +1,22 @@
 /**
  * Custom hash table implementation using separate chaining.
  *
+ * This class provides basic operations such as inserting key-value pairs,
+ * retrieving values by key, removing entries, and checking for the existence
+ * of a value. It uses separate chaining to handle hash collisions.
+ *
  * @param <K> the type of keys
  * @param <V> the type of values
  */
 public class MyHashTable<K, V> {
+    /**
+     * A node class used to store the key-value pair and reference to the next node
+     * in case of collision (separate chaining).
+     */
     private class HashNode<K, V> {
-        private K key;
-        private V value;
-        private HashNode<K, V> next;
+        private K key;    // The key for this entry
+        private V value;  // The value associated with the key
+        private HashNode<K, V> next;  // Reference to the next node in the chain
 
         public HashNode(K key, V value) {
             this.key = key;
@@ -17,19 +25,27 @@ public class MyHashTable<K, V> {
 
         @Override
         public String toString() {
-            return "{" + key + " " + value + "}";
+            return "{" + key + " " + value + "}";  // String representation of the key-value pair
         }
     }
 
-    private HashNode<K, V>[] chainArray;
-    private int M = 11;
-    private int size;
+    private HashNode<K, V>[] chainArray;  // Array of linked lists (chains) for handling collisions
+    private int M = 11;  // The initial number of buckets (can be modified)
+    private int size;  // Number of elements in the hash table
 
+    /**
+     * Default constructor that initializes the hash table with 11 buckets.
+     */
     public MyHashTable() {
         chainArray = new HashNode[M];
         size = 0;
     }
 
+    /**
+     * Constructor that allows specifying the number of buckets for the hash table.
+     *
+     * @param M the number of buckets
+     */
     public MyHashTable(int M) {
         this.M = M;
         chainArray = new HashNode[M];
@@ -37,7 +53,10 @@ public class MyHashTable<K, V> {
     }
 
     /**
-     * Hash function to calculate bucket index.
+     * Hash function that computes the index for a given key.
+     * This function uses the hash code of the key and applies modulo with M
+     * to ensure the index is within bounds.
+     *
      * @param key the key to hash
      * @return the calculated bucket index
      */
@@ -46,22 +65,26 @@ public class MyHashTable<K, V> {
     }
 
     /**
-     * Adds a key-value pair to the hash table.
-     * @param key key of the entry to insert
-     * @param value value to insert
+     * Inserts a new key-value pair into the hash table.
+     * If the key already exists, updates the corresponding value.
+     *
+     * @param key the key of the entry to insert
+     * @param value the value associated with the key
      */
     public void put(K key, V value) {
-        int bucketIndex = hash(key);
+        int bucketIndex = hash(key);  // Find the bucket index using the hash function
         HashNode<K, V> head = chainArray[bucketIndex];
 
+        // Check if the key already exists, and if so, update its value
         while (head != null) {
             if (head.key.equals(key)) {
                 head.value = value;
                 return;
             }
-            head = head.next;
+            head = head.next;  // Traverse the linked list if key is not found
         }
 
+        // If the key doesn't exist, create a new node and insert it at the beginning of the chain
         size++;
         head = chainArray[bucketIndex];
         HashNode<K, V> newNode = new HashNode<>(key, value);
@@ -70,33 +93,37 @@ public class MyHashTable<K, V> {
     }
 
     /**
-     * Retrieves a value for the given key.
-     * @param key the key to look up
+     * Retrieves the value associated with the specified key.
+     *
+     * @param key the key to search for
      * @return the value associated with the key, or null if the key is not found
      */
     public V get(K key) {
-        int bucketIndex = hash(key);
+        int bucketIndex = hash(key);  // Find the bucket index using the hash function
         HashNode<K, V> head = chainArray[bucketIndex];
 
+        // Traverse the linked list to find the key and return its associated value
         while (head != null) {
             if (head.key.equals(key)) {
                 return head.value;
             }
             head = head.next;
         }
-        return null;
+        return null;  // Return null if the key is not found
     }
 
     /**
-     * Removes a key-value pair and returns the removed value.
+     * Removes a key-value pair from the hash table and returns the associated value.
+     *
      * @param key the key to remove
-     * @return the removed value, or null if the key is not found
+     * @return the value associated with the key, or null if the key is not found
      */
     public V remove(K key) {
-        int bucketIndex = hash(key);
+        int bucketIndex = hash(key);  // Find the bucket index using the hash function
         HashNode<K, V> head = chainArray[bucketIndex];
         HashNode<K, V> prev = null;
 
+        // Traverse the linked list to find the key
         while (head != null) {
             if (head.key.equals(key)) {
                 break;
@@ -105,68 +132,75 @@ public class MyHashTable<K, V> {
             head = head.next;
         }
 
+        // Return null if the key is not found
         if (head == null) return null;
 
+        // If the key is found, remove the node
         size--;
         if (prev != null) {
-            prev.next = head.next;
+            prev.next = head.next;  // Remove node by skipping it
         } else {
-            chainArray[bucketIndex] = head.next;
+            chainArray[bucketIndex] = head.next;  // Remove node if it is the first node in the chain
         }
-        return head.value;
+        return head.value;  // Return the removed value
     }
 
     /**
-     * Checks if a value exists in the table.
-     * @param value value to search for
-     * @return true if the value is present, false otherwise
+     * Checks if a particular value exists in the hash table.
+     *
+     * @param value the value to search for
+     * @return true if the value exists in the hash table, false otherwise
      */
     public boolean contains(V value) {
+        // Iterate over all the chains in the hash table
         for (int i = 0; i < M; i++) {
             HashNode<K, V> head = chainArray[i];
             while (head != null) {
                 if (head.value.equals(value)) {
-                    return true;
+                    return true;  // Value found, return true
                 }
-                head = head.next;
+                head = head.next;  // Traverse the linked list
             }
         }
-        return false;
+        return false;  // Return false if value is not found
     }
 
     /**
      * Retrieves the key corresponding to a given value.
-     * @param value value to search for
+     *
+     * @param value the value to search for
      * @return the key corresponding to the value, or null if the value is not found
      */
     public K getKey(V value) {
+        // Iterate over all the chains in the hash table
         for (int i = 0; i < M; i++) {
             HashNode<K, V> head = chainArray[i];
             while (head != null) {
                 if (head.value.equals(value)) {
-                    return head.key;
+                    return head.key;  // Return the key if value is found
                 }
-                head = head.next;
+                head = head.next;  // Traverse the linked list
             }
         }
-        return null;
+        return null;  // Return null if value is not found
     }
 
     /**
-     * Returns an array with sizes of each bucket.
-     * @return an array with bucket sizes
+     * Returns the sizes of each bucket in the hash table as an array.
+     *
+     * @return an array with the number of elements in each bucket
      */
     public int[] bucketSizes() {
-        int[] sizes = new int[M];
+        int[] sizes = new int[M];  // Array to store bucket sizes
         for (int i = 0; i < M; i++) {
             int count = 0;
             HashNode<K, V> current = chainArray[i];
             while (current != null) {
                 count++;
-                current = current.next;
+                current = current.next;  // Count the nodes in the current bucket
             }
-            sizes[i] = count;
+            sizes[i] = count;  // Store the count in the array
         }
-        return sizes;
+        return sizes;  // Return the array with bucket sizes
     }
 }

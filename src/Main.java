62b3110e1baf79a3418ad56cd1afffbd0ea0d2b9
Yerkanat <erkanatm249@gmail.com>
2025-04-28 21:42:22 +0500

    upgrade the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>(20);
+        Random random = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < 10000; i++) {
+            int id = random.nextInt(100000);
+            String name = "Name" + random.nextInt(1000);
+            MyTestingClass key = new MyTestingClass(id, name);
+            Student value = new Student("Student" + id, random.nextInt(30) + 18);
+            table.put(key, value);
+        }
+
+
+        int[] bucketSizes = table.bucketSizes();
+        for (int i = 0; i < bucketSizes.length; i++) {
+            System.out.println("Bucket " + i + ": " + bucketSizes[i] + " elements");
         }
     }
 }
